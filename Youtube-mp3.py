f5d7c395881c1878bd57f0a19fce20758297a8e6
th3linja <th3linja@yahoo.com>
2019-09-04 23:09:45 -0700

    9/4/2019

--- Youtube-mp3.py
+++ Youtube-mp3.py
@@ -2,78 +2,98 @@ import sys
 import youtube_dl
 from PyQt5 import QtWidgets
 from collections import deque
+import threading
 import os
+
 
 # os.system("cmd /c python -m pip install --upgrade pip")
 # os.system("cmd /c pip install --upgrade youtube_dl")
 # os.system("cmd /c pip install --upgrade PyQt5")
-
-queue = deque()
-queue_list = ""
 
 
 class YoutubeMP3(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
-        self.init()
 
-    def init(self):
-
+        self.youtube_download_opts = {
+            'skip_download': True,
+            'outtmpl': 'C:/Users/hyakk/Desktop/Playlist/%(title)s.%(ext)s',
+        }
         self.add = QtWidgets.QPushButton("Add")
         self.download = QtWidgets.QPushButton("Download")
-        self.line = QtWidgets.QLineEdit()
-        self.queue = QtWidgets.QLabel()
+        self.line_add = QtWidgets.QLineEdit()
+        self.edit = QtWidgets.QPushButton("Edit")
+        self.thumbnail = QtWidgets.QLabel()
+        self.queue = deque()
+        self.title = deque()
+        self.gui_title = QtWidgets.QLabel("")
+        self.gui_list = QtWidgets.QLabel("")
+        self.temp = ""
 
-        h_box = QtWidgets.QHBoxLayout()
-        h_box.addStretch()
-        h_box.addWidget(self.add)
-        h_box.addWidget(self.download)
-        h_box.addStretch()
+        h1_box = QtWidgets.QHBoxLayout()
+        h1_box.addWidget(self.gui_list)
+
+        h2_box = QtWidgets.QHBoxLayout()
+        h2_box.addStretch()
+        h2_box.addWidget(self.add)
+        h2_box.addWidget(self.download)
+        h2_box.addStretch()
 
         v_box = QtWidgets.QVBoxLayout()
         v_box.addStretch()
-        v_box.addWidget(self.queue)
-        v_box.addWidget(self.line)
-        v_box.addLayout(h_box)
+        v_box.addWidget(self.line_add)
+        v_box.addLayout(h1_box)
+        v_box.addLayout(h2_box)
         v_box.addStretch()
 
         self.setLayout(v_box)
         self.setWindowTitle("Youtube MP3")
         self.setGeometry(300, 300, 400, 100)
-        self.download.clicked.connect(self.btn_clicked)
-        self.add.clicked.connect(self.btn_clicked)
+        self.download.clicked.connect(self.download_btn)
+        self.add.clicked.connect(self.add_btn)
         self.show()
 
-    def btn_clicked(self):
-        global queue
-        global queue_list
+    def download_btn(self):
         sender = self.sender()
+        re_download_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': 'C:/Users/hyakk/Desktop/Playlist/%(title)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
         if sender.text() == "Download":
-            youtube_download_opts = {
-                'format': 'bestaudio/best',
-                'outtmpl': 'C:/Users/hyakk/Desktop/Playlist/%(title)s.%(ext)s',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192',
-                }],
-            }
+            with youtube_dl.YoutubeDL(re_download_opts) as y_dl:
+                while len(self.queue) > 0:
+                    y_dl.download([self.queue.popleft()])
+                self.gui_list.setText("Download Completed")
 
-            with youtube_dl.YoutubeDL(youtube_download_opts) as y_dl:
-                while not queue:
-                    y_dl.download([queue.popleft()])
-
+    def add_btn(self):
+        sender = self.sender()
         if sender.text() == "Add":
-            if self.line.text() in queue:
+            if self.line_add.text().strip(" ") in self.queue:
                 print("This is already in queue.")
-                self.line.clear()
+                self.line_add.clear()
+            elif self.line_add.text().strip(" ") == "":
+                print("Please enter a valid URL")
             else:
-                queue.append(str(self.line.text()))
-                queue_list += str(self.line.text()) + '\n'
-                self.queue.setText(queue_list)
-                self.line.clear()
-                print(queue)
+                self.gui_title.setText(youtube_dl.YoutubeDL(self.youtube_download_opts)
+                                       .extract_info(self.line_add.text().strip(" "), download=False)['title'])
+                self.temp += str(self.gui_title.text() + '\n' + self.line_add.text().strip(" ")) + '\n'
+                self.gui_list.setText(self.temp)
+                self.queue.append(str(self.line_add.text().strip(" ")))
+                self.title.append(self.gui_title.text().translate(self.gui_title.text().maketrans('\/:*?"<>|', '_________')))
+                self.line_add.clear()
+                print(self.queue)
+                print(self.title)
+
+    def edit_btn(self):
+        sender = self.sender()
+        if sender.text() == "Edit":
+            return
 
 
 sys.except_hook = sys.excepthook
